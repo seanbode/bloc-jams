16755f49cdca4d04ba9c968cb3ff9678cfd2ea08
Sean Bode <seanpaulbode@gmail.com>
2016-11-16 15:33:16 -0500

    a23-dom-scripting-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(index) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
 
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+  forEach(points, revealPoint);
 
   for (var i = 0; i < points.length; i ++) {
     revealPoint();
